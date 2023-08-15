fbf490f97985658abb3afc41dc0703aa45bafb04
Shamal Rodrigo <99328247+IT21307980@users.noreply.github.com>
2023-08-15 16:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+//colour change in when srolliing
+
+// const activePage = window.location.pathname;
+// const navLinks = querySelectorAll('header nav a').
+// forEach(link =>{
+//     if(link.href.includes(`${activePage}`)){
+//         link.classList.add('active');
+//     }
+// })
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;        
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach( links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href* = '+ id +']').classList.add('active');
+            });
+        }
+    })
+    
+   
+}
